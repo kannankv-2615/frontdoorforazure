4855fc38e3f5e89d3ed5c1fa980c13dcfc58e830
kannankv-2615 <68101822+kannankv-2615@users.noreply.github.com>
2020-07-10 20:03:20 +0530

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="amptdb.mysql.database.azure.com",
+  user="Azureadmin@amptdb",
+  password="Password@123",
+  database="mysampledb"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT * FROM customers")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
